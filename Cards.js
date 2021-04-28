5e0cc63a4977eb14be40839f218f2e25f3237d93
Lenora Batcher <lenora.batcher@mymail.champlain.edu>
2021-04-27 20:12:51 -0400

    added functionality, stlying, boat instructions

--- Cards.js
+++ Cards.js
@@ -1,37 +1,58 @@
+const POSSIBLES = ["sheep", "wheat", "stone", "clay", "wood"];
+
 function stealRandomCard(){
     var resources = [];
 
-    var sheep = document.getElementById("sheep").value;
-    var stone = document.getElementById("stone").value;
-    var clay = document.getElementById("clay").value;
-    var wheat = document.getElementById("wheat").value;
-    var wood = document.getElementById("wood").value;
-    console.log(sheep);
+	for (var i = 0; i < POSSIBLES.length; i++) {
+		for (var j = 0; j < document.getElementById(POSSIBLES[i]).value; j++) {
+			resources.push(POSSIBLES[i]);
+		}
+	}
 
-    for (var i = 0; i < sheep; i++){
-        resources.push("sheep");
-    }
-
-    for (var i = 0; i < stone; i++){
-        resources.push("stone");
-    }
-
-    for (var i = 0; i < clay; i++){
-        resources.push("clay");
-    }
-
-    for (var i = 0; i < wood; i++){
-        resources.push("wood");
-    }
-
-    for (var i = 0; i < wheat; i++){
-        resources.push("wheat");
-    }
-    console.log(resources);
-
-    var output = "You have no cards to steal."
+    var output = "You have no cards to steal.";
+	var broke = true;
+	
     if (resources.length > 0){
         output = resources[Math.floor(Math.random() * resources.length)];
+		broke = false;
     }
+	
     document.getElementById("stolen").innerHTML = output;
+	
+	if (!broke) {
+		document.getElementById(output).value--;
+	}
+}
+
+function displayZeros() {
+	var inputs = document.getElementsByTagName("input");
+	
+	for (var i = 0; i < inputs.length; i++) {
+		if (inputs[i].type == "number") {
+			inputs[i].value = 0;
+		}
+	}
+	
+	document.getElementById("vp").innerHTML = 0;
+}
+
+function calcVP(value, id) {
+	var vpElement = document.getElementById("vp");
+	
+	if (document.getElementById(id).value == "off") {
+		vpElement.innerHTML = parseInt(vpElement.innerHTML) + value;
+		document.getElementById(id).value = "on";
+	} else {
+		vpElement.innerHTML = parseInt(vpElement.innerHTML) - value;
+		document.getElementById(id).value = "off";
+	}
+}
+
+function calcVPNum() {
+	var runningTotal = parseInt(document.getElementById("vc").value);
+	
+	if (document.getElementById("road").value == "on") { runningTotal += 2; }
+	if (document.getElementById("army").value == "on") { runningTotal += 2; }
+
+	document.getElementById("vp").innerHTML = runningTotal;
 }
